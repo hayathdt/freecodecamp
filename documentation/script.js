f6d93ae1d065c508a470b70e3fca90e5b5f14a41
hayathdt <hayat.haddouti2@gmail.com>
2025-01-13 15:25:30 +0100

    night mode icon

--- documentation/script.js
+++ documentation/script.js
@@ -1,3 +1,13 @@
 const toggleNightMode = () => {
     document.body.classList.toggle('night-mode');
+    const sunIcon = document.querySelector('.fa-sun');
+    const moonIcon = document.querySelector('.fa-moon');
+    
+    if (document.body.classList.contains('night-mode')) {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline';
+    } else {
+        sunIcon.style.display = 'inline';
+        moonIcon.style.display = 'none';
+    }
 }
